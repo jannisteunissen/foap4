e5404a979e9a92be9c78c355c8f3acfb17069fcd
Jannis Teunissen <jannis@leigong.wlan.cwi.nl>
2026-01-16 10:42:30 +0100

    Make HLL flux a bit more robust

--- src/numerics/flux_hll.f90
+++ src/numerics/flux_hll.f90
@@ -20,26 +20,35 @@ subroutine flux_hll_one_side(flux_dim, i0, u, flux, max_wavespeed)
   real(dp), intent(out) :: max_wavespeed
   real(dp)              :: u_LR(n_tvars, 2)
   real(dp)              :: flux_LR(n_tvars, 2)
-  real(dp)              :: cmin, cmax
+  real(dp)              :: S_L, S_R, dS
+  real(dp), parameter   :: eps = 1e-14_dp
 
   call reconstruct(u, i0, u_LR)
 
   call get_flux(flux_dim, u_LR(:, 1), flux_LR(:, 1))
   call get_flux(flux_dim, u_LR(:, 2), flux_LR(:, 2))
-  call get_min_max_wavespeed(flux_dim, u_LR, cmin, cmax)
-  max_wavespeed = max(abs(cmin), cmax)
+  call get_min_max_wavespeed(flux_dim, u_LR, S_L, S_R)
+  max_wavespeed = max(abs(S_L), abs(S_R))
 
-  ! Convert to conservative
+  ! Convert to conservative for HLL formula
   call to_conservative(u_LR(:, 1))
   call to_conservative(u_LR(:, 2))
 
-  if (cmin >= 0) then
+  if (S_L >= 0.0_dp) then
      flux = flux_LR(:, 1)
-  else if (cmax <= 0) then
+  else if (S_R <= 0.0_dp) then
      flux = flux_LR(:, 2)
   else
-     flux = (cmax * flux_LR(:, 1) - cmin * flux_LR(:, 2) + &
-          cmin * cmax * (u_LR(:, 2) - u_LR(:, 1))) / (cmax - cmin)
+     dS = S_R - S_L
+
+     if (dS > eps * (abs(S_L) + abs(S_R))) then
+        ! Regular case
+        flux = (S_R * flux_LR(:, 1) - S_L * flux_LR(:, 2) + &
+             S_L * S_R * (u_LR(:, 2) - u_LR(:, 1))) / dS
+     else
+        ! Fallback to TVDLF
+        flux = 0.5_dp * (flux_LR(:, 1) + flux_LR(:, 2) - &
+             max_wavespeed * (u_LR(:, 2) - u_LR(:, 1)))
+     end if
   end if
 end subroutine flux_hll_one_side
-
